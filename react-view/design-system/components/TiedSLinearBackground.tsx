7a9ec35a4b3cfb08e1dca9c3fb8e9aa89378f02c
amehmeto <42amehmeto@gmail.com>
2024-02-07 18:11:52 +0300

    fix: statusBarHeight

--- react-view/design-system/components/TiedSLinearBackground.tsx
+++ react-view/design-system/components/TiedSLinearBackground.tsx
@@ -6,27 +6,27 @@ import { Platform, StatusBar, StyleSheet } from 'react-native'
 export function TiedSLinearBackground(
   props: Readonly<{ children: React.ReactNode }>,
 ) {
+  const statusBarHeight =
+    Platform.OS === 'android' ? StatusBar.currentHeight ?? 0 : 0
+
   return (
     <LinearGradient
       colors={[T.color.purple, T.color.lightBlueShade, T.color.lightBlue]}
       start={{ x: 0.4, y: 0.5 }}
       end={{ x: 1.5, y: 0.6 }}
-      style={styles.container}
+      style={[
+        styles.container,
+        { paddingTop: T.spacing.large + statusBarHeight },
+      ]}
     >
       {props.children}
     </LinearGradient>
   )
 }
 
-const statusBarHeight =
-  T.spacing.medium + Platform.OS === 'android'
-    ? StatusBar.currentHeight ?? 0
-    : 0
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: T.spacing.large,
-    statusBarHeight,
   },
 })

35894196cd615000323d7f5381f25c178ee8584c
amehmeto <42amehmeto@gmail.com>
2024-03-22 10:43:06 +0300

    fix: use of createSelector for memoization

--- src/core/blocklist/selectors/selectAllBlocklists.ts
+++ src/core/blocklist/selectors/selectAllBlocklists.ts
@@ -1,5 +1,8 @@
 import { blocklistAdapter } from '../blocklist.ts'
 import { RootState } from '../../_redux_/createStore.ts'
+import { createSelector } from '@reduxjs/toolkit'
 
-export const selectAllBlocklists = (state: RootState) =>
-  blocklistAdapter.getSelectors().selectAll(state.blocklist)
+export const selectAllBlocklists = createSelector(
+  [(state: RootState) => state.blocklist],
+  (blocklists) => blocklistAdapter.getSelectors().selectAll(blocklists),
+)

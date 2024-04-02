82ad2c8e2258365ab69dd3076981830134519ff9
amehmeto <42amehmeto@gmail.com>
2024-04-02 09:37:02 +0300

    feat(BottomTabNavigator): adding Strict Mode tab menu

--- src/react-view/navigators/BottomTabNavigator.tsx
+++ src/react-view/navigators/BottomTabNavigator.tsx
@@ -1,10 +1,12 @@
-import { SettingsScreen } from '../screens/Home/SettingScreen/SettingsScreen.tsx'
+import { SettingsScreen } from '../screens/Settings/SettingScreen/SettingsScreen.tsx'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Entypo, Ionicons } from '@expo/vector-icons'
 import { T } from '../design-system/theme'
 import { BlocklistStackNavigator } from './BlocklistStackNavigator'
 import { TabScreens } from './screen-lists/TabScreens'
 import { HomeStackNavigator } from './HomeStackNavigator.tsx'
+import { StrictModeScreen } from '../screens/StrictMode/StrictModeScreen/StrictModeScreen.tsx'
+import { StyleSheet } from 'react-native'
 
 const Tab = createBottomTabNavigator()
 
@@ -13,12 +15,7 @@ export function BottomTabNavigator() {
     <Tab.Navigator
       screenOptions={{
         tabBarLabelPosition: 'below-icon',
-        tabBarStyle: {
-          borderTopWidth: T.border.width.none,
-          backgroundColor: T.color.darkBlue,
-          height: T.tabBarHeight,
-          padding: T.spacing.small,
-        },
+        tabBarStyle: styles.tabBar,
         tabBarActiveTintColor: T.color.lightBlue,
         tabBarInactiveTintColor: T.color.inactive,
         headerShown: false,
@@ -31,6 +28,15 @@ export function BottomTabNavigator() {
         options={{
           tabBarIcon: ({ color, size }) => (
             <Entypo name="light-up" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={TabScreens.STRICT_MODE}
+        component={StrictModeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="lock-open-outline" size={size} color={color} />
           ),
         }}
       />
@@ -55,3 +61,12 @@ export function BottomTabNavigator() {
     </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    borderTopWidth: T.border.width.none,
+    backgroundColor: T.color.darkBlue,
+    height: T.tabBarHeight,
+    padding: T.spacing.small,
+  },
+})
--- src/react-view/navigators/screen-lists/TabScreens.ts
+++ src/react-view/navigators/screen-lists/TabScreens.ts
@@ -2,4 +2,5 @@ export enum TabScreens {
   HOME = 'Home',
   BLOCKLIST = 'Blocklist',
   SETTINGS = 'Settings',
+  STRICT_MODE = 'Strict Mode',
 }

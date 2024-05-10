655498bbf73fcbdff54c7da2991aa6f44e6f7577
amehmeto <42amehmeto@gmail.com>
2024-05-10 20:05:18 +0300

    not working: different jvm for java and kotlin

---
+++ modules/list-installed-apps/src/ListInstalledAppsView.web.js
@@ -0,0 +1,11 @@
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+var React = require('react')
+function ListInstalledAppsView(props) {
+  return (
+    <div>
+      <span>{props.name}</span>
+    </div>
+  )
+}
+exports.default = ListInstalledAppsView
